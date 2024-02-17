7dfe8d939d6f5aeb535ffcf74da651bd2f857df3
Izyk <lance_hemphill1234@yahoo.com>
2024-02-17 01:46:21 -0500

    added score change

--- index.js
+++ index.js
@@ -18,19 +18,35 @@ const lizard = {
     value: 4,
 }
 
-const options = {
-    rock,
-    paper,
-    scissors
+const outcomes = {
+    winning: {
+        paper: "paperrock",
+        scissors: "scissorspaper",
+        rock: "rockscissors"
+    },
+    losing: {
+        paper: "paperscissors",
+        rock: "rockpaper",
+        scissors: "scissorsrock"
+    },
+    tie: {
+        paper: "paperpaper",
+        scissors: "scissorsscissors",
+        rock: "rockrock"
+    }
 }
 
 
 
 let playBtn = document.getElementById('play-again')
-let resultTxt = document.getElementById('game-result')
+let gameResult = document.getElementById('game-result')
 
 let computerIcon = document.getElementById('computer-icon')
 let userIcon = document.getElementById('user-icon')
+
+let score = document.getElementById('score')
+
+
 
 
 
@@ -41,35 +57,35 @@ computerIcon.classList.add("unselected");
 userIcon.classList.add("unselected");
 
 
-playBtn.classList.add("result-btn-initial")
-resultTxt.classList.add("result-text-inital")
+playBtn.classList.add("result-btn-initial");
+gameResult.classList.add("result-text-inital");
 
-let computerState = 0
-let playerState = 0
-let isActive = false
-
-
+let computerState = 0;
+let playerState = 0;
+let isActive = false;
+let winState = 0;
+let gameScore = 0;
 
 
 
 // Game
 
 
+
 function computerSelect() {
     computerState = Math.floor(Math.random() * (3 - 1 + 1) + 1)
-    console.log(computerState)
     switch(computerState) {
         case computerState = 1:
             computerIcon.classList.add("rock-selected")
-            computerState = rock.value
+            computerState = "rock"
             break;
         case computerState = 2:
             computerIcon.classList.add("paper-selected")
-            computerState = paper.value
+            computerState = "paper"
             break;
         case computerState = 3:
             computerIcon.classList.add("scissors-selected")
-            computerState = scissors.value
+            computerState = "scissors"
             break;
     }
 
@@ -83,64 +99,115 @@ function playerSelect(value) {
     
     switch(value) {
 
-        case value = scissors:
+        case value = scissors.value:
             userIcon.classList.add('scissors-selected')
-            playerState = scissors.value;
+            playerState = "scissors";
             break;
-        case value = paper:
+        case value = paper.value:
             userIcon.classList.add('paper-selected')
-            playerState = paper.value;
+            playerState = "paper";
             break;
-        case value = rock:
-            playerState = rock.value;
+        case value = rock.value:
             userIcon.classList.add('rock-selected')
+            playerState = "rock";
             break;
     }
+    
     computerSelect()
     return isActive = true
 }
 
 
-function startGame() {
 
-    if(playerState = scissors.value && computerState === paper.value) {
+function startGame() {
+    const gameValues = `${playerState}${computerState}`
+    console.log(gameValues)
+    if(gameValues.includes(outcomes.winning.paper) || gameValues.includes(outcomes.winning.rock) || gameValues.includes(outcomes.winning.scissors)) {
         console.log("Winner")
+        winState = 1
+        gameEnd()
+        return
+    }
+    else if (gameValues.includes(outcomes.losing.paper) || gameValues.includes(outcomes.losing.rock) || gameValues.includes(outcomes.losing.scissors)) {
+        console.log("Loser")
+        winState = -1
+        gameEnd()
+        console.log(winState)
+        return
+    }
+    else if (gameValues.includes(outcomes.tie.paper) || gameValues.includes(outcomes.tie.rock) || gameValues.includes(outcomes.tie.scissors)) {
+        console.log("Tie")
+        winState = 0
+        gameEnd()
+        return
     }
     
-    else if(playerState = scissors.value && computerState === rock.value) {
-        console.log("Loser")
+    
+    console.log("loser")
+}
+
+
+function gameEnd() {
+    
+    switch(winState) {
+        case winState = 1:
+            console.log("yes")
+            gameResult.innerText = "Winner"
+            gameResult.classList.add('active-result-text')
+            playBtn.innerText = "Play Again"
+            playBtn.classList.add('active-play-btn')
+
+
+            gameScore + 10
+            score.innerText = gameScore
+            break;
+        case winState = -1:
+            console.log("No")
+            gameResult.innerText = "Loser"
+            gameResult.classList.add('active-result-text')
+            playBtn.innerText = "Play Again"
+            playBtn.classList.add('active-play-btn')
+            break;
+        case winState = 0:
+            console.log("Eh")
+            gameResult.innerText = "Tie"
+            gameResult.classList.add('active-result-text')
+            playBtn.innerText = "Play Again"
+            playBtn.classList.add('active-play-btn')
+            break;
     }
-
-    else if(playerState = paper.value && computerState === rock.value) {
-        console.log("Winner")
-    }
-
-    else if(playerState = paper.value && computerState === scissors.value) {
-        console.log("Winner")
-    }
-
-    else if (playerState = rock.value && computerState === scissors.value) {
-        console.log("Winner")
-    }
-
-    else if(playerState = computerState) {
-        console.log("Tie")
-    }
-
-
 }
 
 
 
+function resetGame() {
+    computerIcon.classList.remove("rock-selected")
+    computerIcon.classList.remove("paper-selected")
+    computerIcon.classList.remove("scissors-selected")
+    userIcon.classList.remove('scissors-selected')
+    userIcon.classList.remove('paper-selected')
+    userIcon.classList.remove('rock-selected')
+    gameResult.classList.remove('active-result-text')
+    playBtn.classList.remove('active-play-btn')
+    computerIcon.classList.add("unselected");
+    userIcon.classList.add("unselected");
+    playBtn.classList.add("result-btn-initial")
+    gameResult.classList.add("result-text-inital")
+
+    computerState = 0
+    playerState = 0
+    isActive = false
+    winState = 0
+
+    console.log('reset')
+}
 
 
 
+playBtn.addEventListener("click", () => resetGame())
 
+scissors.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(scissors.value))
 
+paper.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(paper.value))
 
-
-scissors.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(scissors))
-
-paper.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(paper))
-
-rock.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(rock));
+rock.id.addEventListener("click", () => isActive ? console.log("Already selected") : playerSelect(rock.value));
